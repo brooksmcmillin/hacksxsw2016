ccabe7671a11a93bc8010304269277e01f096051
Brooks McMillin <brooks@brooksmcmillin.com>
2016-03-15 23:17:25 -0500

    Other Stuff

---
+++ view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+    $videoName = $_GET["video"];
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $videoName ?></title>
+    <meta name="description" content="We Are Concerts!">
+    <script src="js/aframe.js"></script>
+</head>
+<body>
+<a-scene stats="true">
+    <a-assets>
+        <video id="video"
+               preload="true"
+               src="media/<?php echo $videoName;?>.mp4"
+               width="360" height="360"
+               autoplay loop="true"
+               crossOrigin="anonymous"></video>
+    </a-assets>
+
+    <a-entity camera look-controls></a-entity>
+    <a-entity material="shader: flat; src: #video"
+              geometry="primitive: sphere; radius: 100" scale="-1 1 1">
+    </a-entity>
+</a-scene>
+</body>
+</html>
